302e475009c1a4134bc4cc799f0cd36562058824
tobou <npasquie@users.noreply.github.com>
2023-09-23 12:58:27 -0400

    contract : compiling base

--- contracts/src/Vault.sol
+++ contracts/src/Vault.sol
@@ -1,8 +1,13 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.21;
+pragma solidity ^0.8.21;
 
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import {ERC4626} from "@openzeppelin/contracts/token/ERC20/extensions/ERC4626.sol";
 
 contract Vault is ERC4626 {
+    IERC20 internal constant ASSET = IERC20(address(0));
 
-} 
+    constructor() ERC4626(ASSET) ERC20("ZeroFi US dollar", "zfUSD") {}
+}
