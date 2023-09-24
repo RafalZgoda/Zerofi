38ea991718cf04b1af9b34cf9dfd4a9257670d60
Nico <nicobdx@icloud.com>
2023-09-24 05:32:48 -0400

    rm creditline2

--- pages/api/creditScore.ts
+++ pages/api/creditScore.ts
@@ -14,13 +14,14 @@ export default async function handler(
   const address = addressCap.toLowerCase();
   // console.log({ req });
   const creditLine = await getCreditLine(address);
-  const creditLine2 = await getCreditLineWithHistoryAndApproves(address);
+  //const creditLine2 = await getCreditLineWithHistoryAndApproves(address);
   console.log({
     response1: creditLine,
     //response2: creditLine2,
   })
-  const formatted = Math.floor(creditLine2);
-  console.log({ creditLine, formatted, creditLine2 });
-
+  //const formatted = Math.floor(creditLine2);
+  //console.log({ creditLine, formatted, creditLine2 });
+  const formatted = Math.floor(creditLine);
+  console.log({ formatted });
   res.status(200).json({ message: formatted.toString() });
 }
