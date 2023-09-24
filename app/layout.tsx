d188a92e9c14714bc3f135bc48d74b71656b7600
Madderstone <anthony.issa@hotmail.fr>
2023-09-23 22:38:17 -0400

    profile

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,14 +7,14 @@ import { PrivyProvider, usePrivy } from "@privy-io/react-auth";
 import { useEffect } from "react";
 import { useRouter } from "next/navigation";
 import { Toaster } from "@/components/ui/toaster";
-import {PrivyWagmiConnector} from '@privy-io/wagmi-connector';
+import { PrivyWagmiConnector } from "@privy-io/wagmi-connector";
 // You can import additional chains from 'wagmi/chains'
 // https://wagmi.sh/react/chains
-import {mainnet, goerli} from '@wagmi/chains';
-import {configureChains} from 'wagmi';
+import { mainnet, goerli } from "@wagmi/chains";
+import { configureChains } from "wagmi";
 // You may replace this with your preferred providers
 // https://wagmi.sh/react/providers/configuring-chains#multiple-providers
-import {publicProvider} from 'wagmi/providers/public';
+import { publicProvider } from "wagmi/providers/public";
 
 // Replace the chains and providers with the ones used by your app.
 // https://wagmi.sh/react/providers/configuring-chains
@@ -53,7 +53,7 @@ export default function RootLayout({
             },
           }}
         >
-          <PrivyWagmiConnector wagmiChainsConfig={configureChainsConfig} >
+          <PrivyWagmiConnector wagmiChainsConfig={configureChainsConfig}>
             <Header />
             {children}
             <img
